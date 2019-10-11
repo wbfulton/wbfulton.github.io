e43b8f565b3353df54a7ca3612bb63d8bff0e726
Will Fulton <wbfulton@me.com>
2019-10-11 12:22:43 -0700

    play/pause gifs

--- js/grayscale.js
+++ js/grayscale.js
@@ -1,15 +1,23 @@
 (function($) {
-  "use strict"; // Start of use strict
+  'use strict'; // Start of use strict
 
   // Smooth scrolling using jQuery easing
   $('a.js-scroll-trigger[href*="#"]:not([href="#"])').click(function() {
-    if (location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') && location.hostname == this.hostname) {
+    if (
+      location.pathname.replace(/^\//, '') ==
+        this.pathname.replace(/^\//, '') &&
+      location.hostname == this.hostname
+    ) {
       var target = $(this.hash);
       target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
       if (target.length) {
-        $('html, body').animate({
-          scrollTop: (target.offset().top - 70)
-        }, 1000, "easeInOutExpo");
+        $('html, body').animate(
+          {
+            scrollTop: target.offset().top - 70
+          },
+          1000,
+          'easeInOutExpo'
+        );
         return false;
       }
     }
@@ -28,15 +36,51 @@
 
   // Collapse Navbar
   var navbarCollapse = function() {
-    if ($("#mainNav").offset().top > 100) {
-      $("#mainNav").addClass("navbar-shrink");
+    if ($('#mainNav').offset().top > 100) {
+      $('#mainNav').addClass('navbar-shrink');
     } else {
-      $("#mainNav").removeClass("navbar-shrink");
+      $('#mainNav').removeClass('navbar-shrink');
     }
   };
+  // Get Gif
+  var getGif = function() {
+    var gif = [];
+    $('img').each(function() {
+      var data = $(this).data('alt');
+      gif.push(data);
+    });
+    return gif;
+  };
+  var gif = getGif();
+
+  // Preload all the GIF.
+  var image = [];
+
+  $.each(gif, function(index) {
+    image[index] = new Image();
+    image[index].src = gif[index];
+  });
+
+  // Stop Gif
+  $('figure').on('click', function() {
+ 
+    var $this   = $(this),
+            $index  = $this.index(),
+             
+            $img    = $this.children('img'),
+            $imgSrc = $img.attr('src'),
+            $imgAlt = $img.attr('data-alt'),
+            $imgExt = $imgAlt.split('.');
+             
+    if($imgExt[1] === 'gif') {
+        $img.attr('src', $img.data('alt')).attr('data-alt', $imgSrc);
+    } else {
+        $img.attr('src', $imgAlt).attr('data-alt', $img.data('alt'));
+    }
+   
+  });
   // Collapse now if page is not at top
   navbarCollapse();
   // Collapse the navbar when page is scrolled
   $(window).scroll(navbarCollapse);
-
 })(jQuery); // End of use strict
